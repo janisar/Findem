2b80e60db2a5aa12aa7c7395131846ca97e5f5b1
Janis <ardeljanis@gmail.com>
2016-11-25 17:15:36 +1100

    added object details view on map

--- app/pages/map/details/ObjectDetails.ts
+++ app/pages/map/details/ObjectDetails.ts
@@ -1,3 +1,20 @@
+import {Component} from "@angular/core";
+import {NavParams} from "ionic-angular";
 /**
  * Created by saarlane on 23/11/16.
  */
+
+@Component({
+  templateUrl: 'build/pages/map/details/ObjectDetails.html',
+  selector: 'map-popup'
+})
+export class ObjectDetails {
+
+  object: any;
+
+  constructor(private navParams: NavParams) {
+    this.object = navParams.get("object");
+
+
+  }
+}
